553a0a1baca0608046be54ea16072df897c9eba8
Midnight Doggo <git@midnightdoggo19.com>
2025-03-03 18:14:47 +0000

    sitemap & robots

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,12 @@ const ip = process.env.IP || 'localhost';
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'assets/favicon.ico')));
+app.set('trust proxy', 1);
+
+const rootLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100
+});
 
 app.get('/api/health', rootLimiter, (req, res) => {
     res.json({
